78d7c2a1905f10786941e10a16603f49f2b64829
Markus Azer <markusgihady@gmail.com>
2026-07-07 23:52:30 +0300

    infra(github): extract vulnerability_alerts to standalone resource

--- infra/github/main.tf
+++ infra/github/main.tf
@@ -16,8 +16,6 @@ resource "github_repository" "repo" {
   delete_branch_on_merge = true
   allow_auto_merge       = true
 
-  vulnerability_alerts = true
-
   # Free on public repos. Private requires GHAS.
   security_and_analysis {
     secret_scanning {
@@ -27,6 +25,11 @@ resource "github_repository" "repo" {
       status = "enabled"
     }
   }
+}
+
+resource "github_repository_vulnerability_alerts" "repo" {
+  repository = github_repository.repo.name
+  enabled    = true
 }
 
 resource "github_repository_dependabot_security_updates" "repo" {
